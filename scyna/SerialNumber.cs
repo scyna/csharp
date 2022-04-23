02e87a18efe33b869b34b92e0c337cab15cf6f3b
NGUYEN Hong San <hongsan@gmail.com>
2022-04-23 22:48:16 +0700

    SerialNumber

--- scyna/SerialNumber.cs
+++ scyna/SerialNumber.cs
@@ -2,35 +2,40 @@ namespace scyna
 {
     class SerialNumber
     {
-        private  string key;
+        private string key;
         private uint prefix = 0;
         private ulong last = 0;
         private ulong next = 0;
 
-        public SerialNumber(string key) {
+        public SerialNumber(string key)
+        {
             this.key = key;
         }
 
         public string Next()
         {
-            lock(this)
+            lock (this)
             {
-            if (next < last) {
-                next++;
-            } else {
-                var request = new proto.GetSNRequest{Key = key};
-                
-                // var response = Service.sendRequest(Path.GEN_GET_SN_URL, request);
-                // if (response != null && response.getCode() == 200) {
-                //     var r = GetSNResponse.parseFrom(response.getBody());
-                //     prefix = r.getPrefix();
-                //     next = r.getStart();
-                //     last = r.getEnd();
-                // }
-            }
-            return String.Format("%d%07d", prefix, next);
+                if (next < last)
+                {
+                    next++;
+                }
+                else
+                {
+                    var request = new proto.GetSNRequest { Key = key };
+
+                    var response = Service.sendRequest(Path.GEN_GET_SN_URL, request);
+                    if (response != null && response.Code == 200)
+                    {
+                        var r = proto.GetSNResponse.Parser.ParseFrom(response.Body);
+                        prefix = r.Prefix;
+                        next = r.Start;
+                        last = r.End;
+                    }
+                }
+                return String.Format("%d%07d", prefix, next);
             }
         }
-        
+
     }
 }
