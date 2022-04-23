2dd49c54c07194e111edf45daf6a3d275cbd42b8
NGUYEN Hong San <hongsan@gmail.com>
2022-04-23 11:07:48 +0700

    add gitignore

--- scyna/Path.cs
+++ scyna/Path.cs
@@ -6,15 +6,15 @@ class Path
     public static String SESSION_UPDATE_CHANNEL = "scyna.session.update";
     public static String SESSION_END_CHANNEL = "scyna.session.end";
     public static String LOG_WRITE_CHANNEL = "scyna.log";
-    public static  String SETTING_WRITE_URL = "/scyna/setting/write";
-    public static  String SETTING_READ_URL = "/scyna/setting/read";
-    public static  String SETTING_REMOVE_URL = "/scyna/setting/remove";
-    public static  String SETTING_UPDATE_CHANNEL = "scyna.setting.updated.";
-    public static  String CALL_WRITE_CHANNEL = "scyna.call.write";
-    public static  String APP_UPDATE_CHANNEL = "scyna.application.updated";
-    public static  String CLIENT_UPDATE_CHANNEL = "scyna.client.updated";
-    public static  String AUTH_CREATE_URL = "/scyna/auth/create";
-    public static  String AUTH_GET_URL = "/scyna/auth/get";
-    public static  String AUTH_LOGOUT_URL = "/scyna/auth/logout"; 
+    public static String SETTING_WRITE_URL = "/scyna/setting/write";
+    public static String SETTING_READ_URL = "/scyna/setting/read";
+    public static String SETTING_REMOVE_URL = "/scyna/setting/remove";
+    public static String SETTING_UPDATE_CHANNEL = "scyna.setting.updated.";
+    public static String CALL_WRITE_CHANNEL = "scyna.call.write";
+    public static String APP_UPDATE_CHANNEL = "scyna.application.updated";
+    public static String CLIENT_UPDATE_CHANNEL = "scyna.client.updated";
+    public static String AUTH_CREATE_URL = "/scyna/auth/create";
+    public static String AUTH_GET_URL = "/scyna/auth/get";
+    public static String AUTH_LOGOUT_URL = "/scyna/auth/logout";
 
 }
