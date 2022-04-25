0a5cc76163032173b6f66f1110f511a9a8fcd0d2
NGUYEN Hong San <hongsan@gmail.com>
2022-04-25 11:47:10 +0700

    minor update

--- scyna/test/Test.cs
+++ scyna/test/Test.cs
@@ -1,51 +1,50 @@
 using NUnit.Framework;
 using Google.Protobuf;
 
-namespace scyna
+namespace scyna;
+
+public class Test
 {
-    public class Test
+    public static void TestService(string url, IMessage request, uint code)
     {
-        public static void TestService(string url, IMessage request, uint code)
+        var res = Service.SendRequest(url, request);
+        Assert.IsNotNull(res);
+        Assert.AreEqual(res.Code, code);
+    }
+
+    public static void TestService<T>(string url, IMessage request, T response, uint code) where T : IMessage<T>, new()
+    {
+        var res = Service.SendRequest(url, request);
+        Assert.IsNotNull(res);
+        Assert.AreEqual(res.Code, code);
+
+        try
         {
-            var res = Service.SendRequest(url, request);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(res.Code, code);
+            MessageParser<T> parser = new MessageParser<T>(() => new T());
+            var r = parser.ParseFrom(res.Body);
+            Assert.IsTrue(response.Equals(r));
         }
-
-        public static void TestService<T>(string url, IMessage request, T response, uint code) where T : IMessage<T>, new()
+        catch
         {
-            var res = Service.SendRequest(url, request);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(res.Code, code);
-
-            try
-            {
-                MessageParser<T> parser = new MessageParser<T>(() => new T());
-                var r = parser.ParseFrom(res.Body);
-                Assert.IsTrue(response.Equals(r));
-            }
-            catch
-            {
-                Assert.Fail();
-            }
+            Assert.Fail();
         }
+    }
 
-        public static T CallService<T>(string url, IMessage request) where T : IMessage<T>, new()
+    public static T CallService<T>(string url, IMessage request) where T : IMessage<T>, new()
+    {
+        var res = Service.SendRequest(url, request);
+        Assert.IsNotNull(res);
+        Assert.AreEqual(res.Code, 200);
+
+        try
         {
-            var res = Service.SendRequest(url, request);
-            Assert.IsNotNull(res);
-            Assert.AreEqual(res.Code, 200);
-
-            try
-            {
-                MessageParser<T> parser = new MessageParser<T>(() => new T());
-                return parser.ParseFrom(res.Body);
-            }
-            catch
-            {
-                Assert.Fail();
-                return default(T);
-            }
+            MessageParser<T> parser = new MessageParser<T>(() => new T());
+            return parser.ParseFrom(res.Body);
+        }
+        catch
+        {
+            Assert.Fail();
+            return default(T);
         }
     }
 }
