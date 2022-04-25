c92b8fc655e9e6d6887a43d50bb15e262b5ad279
NGUYEN Hong San <hongsan@gmail.com>
2022-04-25 10:29:50 +0700

    update testing

--- scyna/test/Test.cs
+++ scyna/test/Test.cs
@@ -5,6 +5,13 @@ namespace scyna
 {
     class Test
     {
+        public static void TestService(string url, IMessage request, uint code)
+        {
+            var res = Service.SendRequest(url, request);
+            Assert.IsNotNull(res);
+            Assert.Equals(res.Code, code);
+        }
+
         public static void TestService<T>(string url, IMessage request, T response, uint code) where T : IMessage<T>, new()
         {
             var res = Service.SendRequest(url, request);
@@ -20,6 +27,24 @@ namespace scyna
             catch
             {
                 Assert.Fail();
+            }
+        }
+
+        public static T CallService<T>(string url, IMessage request) where T : IMessage<T>, new()
+        {
+            var res = Service.SendRequest(url, request);
+            Assert.IsNotNull(res);
+            Assert.Equals(res.Code, 200);
+
+            try
+            {
+                MessageParser<T> parser = new MessageParser<T>(() => new T());
+                return parser.ParseFrom(res.Body);
+            }
+            catch
+            {
+                Assert.Fail();
+                return default(T);
             }
         }
     }
