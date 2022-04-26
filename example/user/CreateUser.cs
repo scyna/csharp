a4ce8aa1661d48e376144b810efa415ec83a9cfb
NGUYEN Hong San <hongsan@gmail.com>
2022-04-26 22:18:05 +0700

    validator

--- example/user/CreateUser.cs
+++ example/user/CreateUser.cs
@@ -1,14 +1,17 @@
 namespace ex.User;
 using scyna;
+using FluentValidation;
 
 public class CreateUser : Service.StatefulHandler<proto.CreateUserRequest>
 {
+    UserValidator validator = new UserValidator();
     public override void Execute()
     {
         Console.WriteLine("Receive CreateUserRequest");
         var userDB = dao.User.DB();
         try
         {
+            if (!validator.Validate(request.User).IsValid) throw new dao.DBException(scyna.Error.REQUEST_INVALID);
             if (userDB.Exist(LOG, request.User.Email)) throw new dao.DBException(dao.Error.USER_EXIST);
             var user = dao.User.FromProto(request.User);
             user.ID = Engine.ID.Next();
@@ -18,5 +21,15 @@ public class CreateUser : Service.StatefulHandler<proto.CreateUserRequest>
         {
             Error(e.Error);
         }
+    }
+}
+
+public class UserValidator : AbstractValidator<proto.User>
+{
+    public UserValidator()
+    {
+        RuleFor(u => u.Name).NotNull();
+        RuleFor(u => u.Email).NotNull().EmailAddress();
+        RuleFor(u => u.Password).NotNull().Length(5, 10);
     }
 }
