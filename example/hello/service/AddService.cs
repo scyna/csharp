db649b755e6373cfbaa692a80ae02633e3ab9d9e
NGUYEN Hong San <hongsan@gmail.com>
2023-02-04 22:37:41 +0700

    hello example

--- example/hello/service/AddService.cs
+++ example/hello/service/AddService.cs
@@ -7,7 +7,11 @@ public class AddService : Endpoint.Handler<proto.AddRequest>
     public override void Execute()
     {
         LOG.Info("Receive AddRequest");
+
         var sum = request.A + request.B;
         if (sum > 100) throw scyna.Error.REQUEST_INVALID;
+
+        Response(new proto.AddResponse { Sum = sum });
+
     }
 }
