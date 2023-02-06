a4a44eb510dfd53da9a57a01f38fdfdaffa85613
NGUYEN Hong San <hongsan@gmail.com>
2023-02-06 14:38:07 +0700

    Task worked

--- example/account/event/AccountCreatedHandler.cs
+++ example/account/event/AccountCreatedHandler.cs
@@ -8,6 +8,11 @@ public class AccountCreatedHandler : Event.Handler<proto.AccountCreated>
     {
         context.Info("Receive AccountCreated event");
         Console.WriteLine(data);
-        /* TODO: do something here*/
+        try
+        {
+            context.ScheduleTask(Path.SEND_EMAIL_CHANNEL, DateTimeOffset.Now.ToUnixTimeSeconds(), 61,
+                    new proto.SendEmailTask { Email = data.Email, Content = "Just say hello" }, 2);
+        }
+        catch (scyna.Error e) { Console.WriteLine(e); }
     }
 }
