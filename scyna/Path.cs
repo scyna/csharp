48b74c9aec0727857fea52de2e1ed47031c0640d
NGUYEN Hong San <hongsan@gmail.com>
2023-06-07 12:13:29 +0700

    use namespace scyna2

--- scyna/Path.cs
+++ scyna/Path.cs
@@ -2,25 +2,25 @@ namespace scyna;
 
 class Path
 {
-    public static string GEN_GET_ID_URL = "/scyna/generator/id";
-    public static string GEN_GET_SN_URL = "/scyna/generator/sn";
-    public static string SESSION_CREATE_URL = "/scyna/session/create";
-    public static string SESSION_UPDATE_CHANNEL = "scyna.session.update";
-    public static string SESSION_END_CHANNEL = "scyna.session.end";
-    public static string LOG_CREATED_CHANNEL = "scyna.log";
-    public static string SETTING_WRITE_URL = "/scyna/setting/write";
-    public static string SETTING_READ_URL = "/scyna/setting/read";
-    public static string SETTING_REMOVE_URL = "/scyna/setting/remove";
-    public static string SETTING_UPDATE_CHANNEL = "scyna.setting.updated.";
-    public static string SETTING_REMOVE_CHANNEL = "scyna.setting.removed.";
-    public static string CALL_WRITE_CHANNEL = "scyna.call.write";
-    public static string APP_UPDATE_CHANNEL = "scyna.application.updated";
-    public static string CLIENT_UPDATE_CHANNEL = "scyna.client.updated";
-    public static string AUTH_CREATE_URL = "/scyna/auth/create";
-    public static string AUTH_GET_URL = "/scyna/auth/get";
-    public static string AUTH_LOGOUT_URL = "/scyna/auth/logout";
-    public static string TRACE_CREATED_CHANNEL = "scyna.trace";
-    public static string START_TASK_URL = "/scyna/task/start";
-    public static string STOP_TASK_URL = "/scyna/task/stop";
+    public static string GEN_GET_ID_URL = "/scyna2/generator/id";
+    public static string GEN_GET_SN_URL = "/scyna2/generator/sn";
+    public static string SESSION_CREATE_URL = "/scyna2/session/create";
+    public static string SESSION_UPDATE_CHANNEL = "scyna2.session.update";
+    public static string SESSION_END_CHANNEL = "scyna2.session.end";
+    public static string LOG_CREATED_CHANNEL = "scyna2.log";
+    public static string SETTING_WRITE_URL = "/scyna2/setting/write";
+    public static string SETTING_READ_URL = "/scyna2/setting/read";
+    public static string SETTING_REMOVE_URL = "/scyna2/setting/remove";
+    public static string SETTING_UPDATE_CHANNEL = "scyna2.setting.updated.";
+    public static string SETTING_REMOVE_CHANNEL = "scyna2.setting.removed.";
+    public static string CALL_WRITE_CHANNEL = "scyna2.call.write";
+    public static string APP_UPDATE_CHANNEL = "scyna2.application.updated";
+    public static string CLIENT_UPDATE_CHANNEL = "scyna2.client.updated";
+    public static string AUTH_CREATE_URL = "/scyna2/auth/create";
+    public static string AUTH_GET_URL = "/scyna2/auth/get";
+    public static string AUTH_LOGOUT_URL = "/scyna2/auth/logout";
+    public static string TRACE_CREATED_CHANNEL = "scyna2.trace";
+    public static string START_TASK_URL = "/scyna2/task/start";
+    public static string STOP_TASK_URL = "/scyna2/task/stop";
 
 }
--- scyna/publish.sh
+++ scyna/publish.sh
@@ -1,4 +1,4 @@
 dotnet nuget remove source "github"
 dotnet nuget add source --name "github" "https://nuget.pkg.github.com/scyna/index.json" --username hongsan --password "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" --store-password-in-clear-text
 dotnet pack --configuration Release
-dotnet nuget push "bin/Release/Scyna.Net.1.0.6.nupkg" --source "github"
+dotnet nuget push "bin/Release/Scyna.Net.1.1.0.nupkg" --source "github"
