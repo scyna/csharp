6c9c8577612ae3f00ca9184d7da024d5dea0a1b6
NGUYEN Hong San <hongsan@gmail.com>
2023-09-30 13:52:35 +0700

    minor update

--- example/hello/Main.cs
+++ example/hello/Main.cs
@@ -1,12 +1,4 @@
-namespace ex.hello;
-
 using scyna;
 
-class Hello
-{
-    static void Main(string[] args)
-    {
-        Engine.Init("http://127.0.0.1:8081", "scyna_test", "123456");
-        Engine.Start();
-    }
-}
+Engine.Init("http://127.0.0.1:8081", "scyna_test", "123456");
+Engine.Start();
